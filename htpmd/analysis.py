59c04546f3e5dfb9b89e58d4ccae38db412f11ad
Tian Xie <txie@txie@mit.edu>
2021-02-03 00:46:04 -0500

    Refactored the rest of the functions

--- htpmd/analysis.py
+++ htpmd/analysis.py
@@ -5,7 +5,9 @@ from .utils import load_lammps
 from pymatgen.core.structure import Structure
 from htpmd.constants import ATOM_MASSES
 from htpmd.trajectory.load import LammpsTrajectoryLoader
-from htpmd.shared.polymer import compute_diffusivity, compute_polymer_diffusivity
+from htpmd.shared.polymer import (
+    compute_diffusivity, compute_polymer_diffusivity, compute_molarity,
+    compute_conductivity, compute_msd_curve, get_cif_at_frame)
 
 
 REQUIRED_METADATA = {
@@ -31,15 +33,11 @@ def get_all_properties(dir_name):
     results['li_diffusivity'] = compute_diffusivity(traj, target_type=90)
     results['tfsi_diffusivity'] = compute_diffusivity(traj, target_type=93)
     results['poly_diffusivity'] = compute_polymer_diffusivity(traj)
-    results['conductivity'] = get_conductivity(
-        traj.lattices, traj.raw_types, traj.unwrapped_coords, pop_mat)
-    results['molarity'] = get_molarity(traj.raw_types, traj.atom_types)
-    results['li_msd_curve'] = get_msd_curve(
-        traj.raw_types, traj.unwrapped_coords, target_type=90)
-    results['tfsi_msd_curve'] = get_msd_curve(
-        traj.raw_types, traj.unwrapped_coords, target_type=93)
-    results['structure'] = get_cif_at_frame(
-        traj.wrapped_coords, traj.lattices, traj.atom_types, k=0)
+    results['conductivity'] = compute_conductivity(traj, pop_mat=pop_mat)
+    results['molarity'] = compute_molarity(traj)
+    results['li_msd_curve'] = compute_msd_curve(traj, target_type=90)
+    results['tfsi_msd_curve'] = compute_msd_curve(traj, target_type=93)
+    results['structure'] = get_cif_at_frame(traj, k=0)
     return results
 
 
@@ -50,120 +48,10 @@ def get_metadata(dir_name):
     return metadata
 
 
-def get_raw_traj(dir_name):
-    """Load the entire trajectory into memory."""
-    wrapped_coords, lattices, types, atom_types, unwrapped_coords = load_lammps(
-        os.path.join(dir_name, 'traj.lammpstrj'))
-    return wrapped_coords, lattices, types, atom_types, unwrapped_coords
-
-
 def get_population_matrix(dir_name):
     """Load the population matrix computed by lammps."""
     pop_mat = np.loadtxt(os.path.join(dir_name, 'population.txt'))
     return pop_mat
-
-
-def get_molarity(raw_types, real_types):
-    """Molarity of the polymer/salt mixture (unit: mol Li / kg polymer)."""
-
-    poly_idx = np.nonzero(raw_types < 89)[0]
-    li_idx = np.nonzero(real_types == 3)[0]
-
-    atom_masses = np.array(ATOM_MASSES)
-
-    poly_mass = np.sum(atom_masses[real_types[poly_idx]])
-
-    return float(len(li_idx)) / poly_mass * 1e3
-
-
-def compute_center_of_mass(coords, atom_types):
-    element_masses = np.array(ATOM_MASSES)
-    atom_masses = element_masses[atom_types]
-    return (np.sum(coords * atom_masses[np.newaxis, :, np.newaxis], axis=1) /
-            np.sum(atom_masses))
-
-
-def get_coords_without_drift(coords, atom_types):
-    cos_coord = compute_center_of_mass(coords, atom_types)
-    return coords - cos_coord[:, np.newaxis]
-
-
-def get_diffusivity(raw_types, unwrapped_coords, target_type):
-    """Diffusivity of a specified atom type (unit: cm^2/s)."""
-    target_idx = np.nonzero(raw_types == target_type)[0]
-    target_coords = unwrapped_coords[:, target_idx]
-    msd = np.mean(np.sum((target_coords[-1] - target_coords[0])**2, axis=-1))
-    return msd / (len(target_coords) - 1) / 6 * 5e-5  # cm^2/s
-
-
-def get_polymer_diffusivity(raw_types, atom_types, unwrapped_coords):
-    # TODO: should F be included?
-    solvate_types = (atom_types == 7) | (atom_types == 8) | (atom_types == 16)
-    poly_solvate_types = (raw_types < 90) & solvate_types
-    poly_solvate_idx = np.nonzero(poly_solvate_types)[0]
-    target_coords = unwrapped_coords[:, poly_solvate_idx]
-    msd = np.mean(np.sum((target_coords[-1] - target_coords[0])**2, axis=-1))
-    return msd / (len(target_coords) - 1) / 6 * 5e-5  # cm^2/s
-
-
-def get_msd_curve(raw_types, unwrapped_coords, target_type):
-    """Computes Mean Squared Dispacement curve (unit: ns, A^2)"""
-    target_idx = np.nonzero(raw_types == target_type)[0]
-    target_coords = unwrapped_coords[:, target_idx]
-
-    ts = np.linspace(1, target_coords.shape[0] - 1, 100, dtype=int)
-    msds = np.array([
-        np.mean(np.sum((target_coords[t:] - target_coords[:-t])**2, axis=-1))
-        for t in ts])
-    # Convert to ns
-    ts = ts * 2e-3
-    return ts, msds
-
-
-def get_conductivity(lattices, raw_types, unwrapped_coords, pop_mat):
-    """Total conducitivty of the system (unit: S/cm)."""
-    max_cluster = 10
-    e_const = 1.6021766209e-19
-    kb_const = 1.38064852e-23
-    T = 353.0
-
-    li_diff = get_diffusivity(
-        raw_types, unwrapped_coords, target_type=90)  # cm^2/s
-    tfsi_diff = get_diffusivity(
-        raw_types, unwrapped_coords, target_type=93)  # cm^2/s
-
-    assert np.isclose(lattices[0:1], lattices).all()
-
-    V = np.prod(lattices[0]) * 1e-24  # cm^3
-
-    cond = 0.
-    total_ion = 0.
-
-    for i in range(max_cluster):
-        for j in range(max_cluster):
-            if i > j:
-                cond += e_const**2 / V / kb_const / T * \
-                    (i - j)**2 * pop_mat[i, j] * li_diff
-            elif i < j:
-                cond += e_const**2 / V / kb_const / T * \
-                    (i - j)**2 * pop_mat[i, j] * tfsi_diff
-            else:
-                pass
-            total_ion += (i + j) * pop_mat[i, j]
-
-    return cond  # S/cm
-
-
-def get_cif_at_frame(wrapped_coords, lattices, atom_types, k):
-    """Computes the cif text representation of the structure at frame k."""
-    structure = Structure(
-        lattice=np.diag(lattices[k]),
-        species=atom_types,
-        coords=wrapped_coords[k],
-        coords_are_cartesian=True,
-        to_unit_cell=True
-    )
-    return structure.to('cif')
 
 
 def lammpstraj2npz(dir_name, out_dir, target_atom_num):
--- htpmd/shared/polymer.py
+++ htpmd/shared/polymer.py
@@ -1,9 +1,11 @@
 """
-Module for methods to compute diffusivity.
+Module for methods to compute properties for the polymer electrolytes.
 """
 import numpy as np
 
 from htpmd.shared.utils import check_params
+from htpmd.constants import ATOM_MASSES
+from pymatgen.core.structure import Structure
 
 
 def compute_diffusivity(trajectory, **params):
@@ -62,7 +64,7 @@ def compute_polymer_diffusivity(trajectory, **params):
                                                         Required fields:
 
     Returns:
-        float:                                          diffusivity (for given target_type)
+        float:                                          diffusivity (for polymer)
 
     """
     required_parameters = tuple()
@@ -77,3 +79,169 @@ def compute_polymer_diffusivity(trajectory, **params):
     target_coords = trajectory.unwrapped_coords[:, poly_solvate_idx]
     msd = np.mean(np.sum((target_coords[-1] - target_coords[0])**2, axis=-1))
     return msd / (len(target_coords) - 1) / 6 * 5e-5  # cm^2/s
+
+
+def compute_molarity(trajectory, **params):
+    """
+    Description:
+        Molarity of the polymer/salt mixture (unit: mol Li / kg polymer).
+
+    Version: 1.0.0
+
+    Author:
+        Name:                                           Tian Xie
+        Affiliation:                                    MIT
+        Email:                                          <optional>
+
+    Args:
+        trajectory (trajectory.base.Trajectory):        trajectory to compute metric on
+        **params:                                       Methodology specific parameters.
+                                                        Required fields:
+
+    Returns:
+        float:                                          molarity
+
+    """
+    required_parameters = tuple()
+    check_params(required_parameters, params)
+
+    poly_idx = np.nonzero(trajectory.raw_types < 89)[0]
+    li_idx = np.nonzero(trajectory.atom_types == 3)[0]
+
+    atom_masses = np.array(ATOM_MASSES)
+
+    poly_mass = np.sum(atom_masses[trajectory.atom_types[poly_idx]])
+
+    return float(len(li_idx)) / poly_mass * 1e3
+
+
+def compute_conductivity(trajectory, **params):
+    """
+    Description:
+        Compute the conductivity using the cluster-Nernst-Einstein described
+        in the following paper (unit: S/cm).
+
+        France-Lanord and Grossman. "Correlations from ion pairing and the
+        Nernst-Einstein equation." Physical review letters 122.13 (2019): 136001.
+
+    Version: 1.0.0
+
+    Author:
+        Name:                                           Tian Xie
+        Affiliation:                                    MIT
+        Email:                                          <optional>
+
+    Args:
+        trajectory (trajectory.base.Trajectory):        trajectory to compute metric on
+        **params:                                       Methodology specific parameters.
+                                                        Required fields:
+                                                            pop_mat: np.array
+
+    Returns:
+        float:                                          conductivity
+
+    """
+    required_parameters = ('pop_mat', )
+    check_params(required_parameters, params)
+
+    max_cluster = 10
+    e_const = 1.6021766209e-19
+    kb_const = 1.38064852e-23
+    T = 353.0
+    pop_mat = params['pop_mat']
+
+    li_diff = compute_diffusivity(trajectory, target_type=90)  # cm^2/s
+    tfsi_diff = compute_diffusivity(trajectory, target_type=93)  # cm^2/s
+
+    assert np.isclose(trajectory.lattices[0:1], trajectory.lattices).all()
+
+    V = np.prod(trajectory.lattices[0]) * 1e-24  # cm^3
+
+    cond = 0.
+    total_ion = 0.
+
+    for i in range(max_cluster):
+        for j in range(max_cluster):
+            if i > j:
+                cond += e_const**2 / V / kb_const / T * \
+                    (i - j)**2 * pop_mat[i, j] * li_diff
+            elif i < j:
+                cond += e_const**2 / V / kb_const / T * \
+                    (i - j)**2 * pop_mat[i, j] * tfsi_diff
+            else:
+                pass
+            total_ion += (i + j) * pop_mat[i, j]
+
+    return cond  # S/cm
+
+
+def compute_msd_curve(trajectory, **params):
+    """
+    Description:
+        Computes Mean Squared Dispacement curve (unit: ns, A^2)
+
+    Version: 1.0.0
+
+    Author:
+        Name:                                           Tian Xie
+        Affiliation:                                    MIT
+        Email:                                          <optional>
+
+    Args:
+        trajectory (trajectory.base.Trajectory):        trajectory to compute metric on
+        **params:                                       Methodology specific parameters.
+                                                        Required fields:
+                                                            target_type (int)
+
+    Returns:
+        ts:                                              time series (np.array)
+        msds:                                            mean squared displacement (np.array)
+    """
+
+    required_parameters = ('target_type', )
+    check_params(required_parameters, params)
+
+    target_idx = np.nonzero(trajectory.raw_types == params['target_type'])[0]
+    target_coords = trajectory.unwrapped_coords[:, target_idx]
+
+    ts = np.linspace(1, target_coords.shape[0] - 1, 100, dtype=int)
+    msds = np.array([
+        np.mean(np.sum((target_coords[t:] - target_coords[:-t])**2, axis=-1))
+        for t in ts])
+    # Convert to ns
+    ts = ts * 2e-3
+    return ts, msds
+
+
+def get_cif_at_frame(trajectory, **params):
+    """
+    Description:
+        Computes the cif text representation of the structure at frame k.
+
+    Version: 1.0.0
+
+    Author:
+        Name:                                           Tian Xie
+        Affiliation:                                    MIT
+        Email:                                          <optional>
+
+    Args:
+        trajectory (trajectory.base.Trajectory):        trajectory to compute metric on
+        **params:                                       Methodology specific parameters.
+                                                        Required fields:
+                                                            k (int)
+
+    Returns:
+
+    """
+    required_parameters = ('k', )
+    check_params(required_parameters, params)
+
+    structure = Structure(
+        lattice=np.diag(trajectory.lattices[params['k']]),
+        species=trajectory.atom_types,
+        coords=trajectory.wrapped_coords[params['k']],
+        coords_are_cartesian=True,
+        to_unit_cell=True
+    )
+    return structure.to('cif')
