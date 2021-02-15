07a2c994888d6204db1678abbcc1652900380d2f
daniel.schweigert <daniel.schweigert@tri.global>
2021-02-15 09:53:00 -0800

    move material target type numbers to constants module

--- htpmd/analysis.py
+++ htpmd/analysis.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from .utils import load_lammps
 from pymatgen.core.structure import Structure
-from htpmd.constants import ATOM_MASSES
+from htpmd.constants import TargetType
 from htpmd.trajectory.load import (
     LammpsTrajectoryLoader, get_metadata, get_population_matrix)
 from htpmd.shared.polymer import (
@@ -21,13 +21,13 @@ def get_all_properties(dir_name):
     results = dict()
     results.update(metadata)
     traj.remove_drift()
-    results['li_diffusivity'] = compute_diffusivity(traj, target_type=90)
-    results['tfsi_diffusivity'] = compute_diffusivity(traj, target_type=93)
+    results['li_diffusivity'] = compute_diffusivity(traj, target_type=TargetType.LI)
+    results['tfsi_diffusivity'] = compute_diffusivity(traj, target_type=TargetType.TFSI)
     results['poly_diffusivity'] = compute_polymer_diffusivity(traj)
     results['conductivity'] = compute_conductivity(traj, pop_mat=pop_mat)
     results['molarity'] = compute_molarity(traj)
-    results['li_msd_curve'] = compute_msd_curve(traj, target_type=90)
-    results['tfsi_msd_curve'] = compute_msd_curve(traj, target_type=93)
+    results['li_msd_curve'] = compute_msd_curve(traj, target_type=TargetType.LI)
+    results['tfsi_msd_curve'] = compute_msd_curve(traj, target_type=TargetType.TFSI)
     results['structure'] = get_cif_at_frame(traj, k=0)
     return results
 
