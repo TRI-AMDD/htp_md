c26cbea7fe28084f31d88df1957be5bcbf66c04e
Ha-Kyung Kwon <ha-kyung.kwon@tri.global>
2021-05-04 11:25:51 -0400

    change molarity to molality

--- htpmd/analysis_test.py
+++ htpmd/analysis_test.py
@@ -14,7 +14,7 @@ def test_get_all_properties(dir_name):
     results = get_all_properties(dir_name)
     property_list = [
         'li_diffusivity', 'tfsi_diffusivity', 'poly_diffusivity',
-        'conductivity', 'molarity', 'li_msd_curve', 'tfsi_msd_curve',
+        'conductivity', 'molality', 'li_msd_curve', 'tfsi_msd_curve',
         'structure', 'mol_smiles', 'poly_smiles', 'force_field', 'material_group',
         'temperature', 'time_step', 'cation_raw_type', 'anion_raw_type',
         'polymer_raw_type_range', 'polymer_solvate_types', 'salt_cation', 'salt_anion',
