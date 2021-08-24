d915f23a0ed69ddeaa45cecd9cbe507dfbaa705c
Tian Xie <txie@mit.edu>
2021-08-24 01:07:21 -0400

    Fixed bug

--- htpmd/ml_models/gnn_test.py
+++ htpmd/ml_models/gnn_test.py
@@ -4,12 +4,12 @@ from htpmd.ml_models import gnn
 @pytest.mark.parametrize(
     'prop',
     [
-        ('conductivity',),
-        ('li_diffusivity',),
-        ('poly_diffusivity',),
-        ('tfsi_diffusivity',),
-        ('molarity',),
-        ('transference_number',),
+        'conductivity',
+        'li_diffusivity',
+        'poly_diffusivity',
+        'tfsi_diffusivity',
+        'molarity',
+        'transference_number',
     ])
 def test_gnn(prop):
-    preds = gnn.predict(['CC(CCNCC(C)OC(=O)[Au])CCO[Cu]'], 'poly_diffusivity')
+    preds = gnn.predict(['CC(CCNCC(C)OC(=O)[Au])CCO[Cu]'], prop)
