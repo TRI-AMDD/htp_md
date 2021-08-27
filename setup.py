2e9bceb1de57a9d40bbf61e4e01237344299a32c
Ha-Kyung Kwon <ha-kyung.kwon@tri.global>
2021-08-27 11:05:11 -0700

    clean up and change version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(readme_path, "r") as f:
 
 setup(
     name='htpmd',
-    version='0.1.5',
+    version='0.1.6',
     description='A library to analyze trajectory data from Molecular Dynamics Simulations',
     long_description=long_description,
     long_description_content_type='text/markdown',
