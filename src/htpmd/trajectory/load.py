2c87fbd266b8a5572d7674da93ab624316852647
arashkhajeh-AMDD <85306077+arashkhajeh-AMDD@users.noreply.github.com>
2022-03-25 14:49:30 -0700

    save population matrix files in local directory as well
    I noticed received an error when testing a directory that did not have a population.txt. The reason is that we need a population matrix to calculate conductivity. Also, the htpmd should work fine on local directories alone. So, I added a couple of lines to save the population matrices in the local directory.
    
    I noticed that this is not an efficient way to calculate the population matrix since it is very time-consuming. The best approach is to rewrite and modify the python2 code to have the same calculations in python3. I will take a look at it to see if I can do it without the pizza package.

--- src/htpmd/trajectory/load.py
+++ src/htpmd/trajectory/load.py
@@ -62,8 +62,11 @@ def get_population_matrix(dir_name):
     file_ref_path = os.path.join(dir_name, 'population.txt')
     if os.path.exists(file_ref_path):
         pop_mat = np.loadtxt(file_ref_path)
-        stacked_pop_mat = []
+        stacked_pop_mat = np.empty((0, 1))
     else:
         stacked_pop_mat, pop_mat = generate_population_matrix(dir_name)
+        # save the population matrices in the local directory
+        np.savetxt(os.path.join(dir_name, 'population.txt'), pop_mat)
+        np.save(os.path.join(dir_name, 'stacked_pop'), stacked_pop_mat)
 
     return stacked_pop_mat, pop_mat
