bc4c55c177797be562e07e28a6e428eb15c1183a
danielschweigert-TRI <daniel.schweigert@tri.global>
2023-02-23 14:20:23 +0000

    repo reorganisation: move unit tests into designated tests folder

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,14 +1,15 @@
-import json
-import os
 import pytest
+from os.path import join, dirname
 from htpmd.analysis import get_all_properties
+
+TEST_DATA_FOLDER = join(dirname(__file__), 'test_data')
 
 
 @pytest.mark.parametrize(
     'dir_name',
     [
-        ('test_data/9-0-246295613-0'),
-        ('test_data/9-0-413610210-0'),
+        (join(TEST_DATA_FOLDER, '9-0-246295613-0')),
+        (join(TEST_DATA_FOLDER, '9-0-413610210-0')),
     ])
 def test_get_all_properties(dir_name):
     results = get_all_properties(dir_name)
